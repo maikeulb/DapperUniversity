5f34e49ac6ac0cbe5838cbbae7f54d67fbac1260
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-25 21:25:13 -0500

    add vm annotation

--- src/ViewModels/EnrollmentDateGroupViewModel.cs
+++ src/ViewModels/EnrollmentDateGroupViewModel.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace DapperUniversity.ViewModels
 {
     public class EnrollmentDateGroupViewModel
     {
+        [DataType(DataType.Date)]
         public DateTime? EnrollmentDate { get; set; }
         public int StudentCount { get; set; }
     }
